2e2b43393c120d6ac1009ac6403a9acc9ad4676e
Erik Stafford <estafford2@ad.davenport.edu>
2019-01-17 19:29:44 -0500

    Add project files.

---
+++ EightQueensProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightQueensProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
