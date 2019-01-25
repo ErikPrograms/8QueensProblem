1be88ea674919806f877c53ae3c4f0801b9e94fe
ErikPrograms <erikdstafford@gmail.com>
2019-01-24 19:15:41 -0500

    Created Grid Class

---
+++ EightQueensProgram/Grid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightQueensProgram
+{
+    public class Grid
+    {
+        private int height;
+        private int width;
+        public int[] Spaces;
+
+        public Grid BuildGrid(int Height, int Width)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
