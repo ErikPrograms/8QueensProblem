da0633c1a6a757d48f24655ce234da12d8d17382
ErikPrograms <erikdstafford@gmail.com>
2019-02-14 19:38:32 -0500

    Added toString to the grid class.

--- EightQueensProgram/Grid.cs
+++ EightQueensProgram/Grid.cs
@@ -10,14 +10,52 @@ namespace EightQueensProgram
     {
         private int height;
         private int width;
-        public int[,] Spaces;
+        private bool[,] Spaces;
 
         public Grid(int Height, int Width)
         {
             height = Height;
             width = Width;
 
-            Spaces = new int[height,width];
+            Spaces = new bool[height,width];
         }
+
+        public void Place(int Height, int Width)
+        {
+            Spaces[Height - 1, Width - 1] = true;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder outString = new StringBuilder();
+            outString.Append("  ");
+            for(int i = 1; i<=width; i++)
+            {
+                outString.Append(" " + i + " ");
+            }
+            outString.AppendLine();
+
+            for(int loopcount = 0; loopcount < height; loopcount++)
+            {
+                outString.Append((loopcount + 1) + " ");
+                for(int innerloopcount = 0; innerloopcount < width; innerloopcount ++)
+                {
+                    if(Spaces[loopcount,innerloopcount] == true)
+                    {
+                        outString.Append("|X|");
+                    }
+                    else
+                    {
+                        outString.Append("| |");
+                    }
+                }
+                outString.AppendLine();
+            }
+
+            return outString.ToString();
+        }
+
+
     }
 }
--- EightQueensProgram/Program.cs
+++ EightQueensProgram/Program.cs
@@ -10,8 +10,18 @@ namespace EightQueensProgram
     {
         static void Main(string[] args)
         {
-            //test program 
-            // is this working
+            int GridHeight = 8;
+            int GridWidth = 8;
+            Console.Title = "Queens Program";
+            Console.SetWindowSize(60, 32);
+            Grid testGrind = new Grid(GridHeight, GridWidth);
+            testGrind.Place(2, 2);
+
+            Console.WriteLine(testGrind.ToString());
+
         }
-    }
+
+        }
 }
+
+
